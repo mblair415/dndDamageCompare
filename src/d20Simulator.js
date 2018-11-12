501bea86c363f1af3df713b3ab37cbcc4a098e59
mblair415 <mblair415@gmail.com>
2018-11-12 13:20:41 -0800

    finalized functionality.  added d20Simulator.js to show that process

---
+++ src/d20Simulator.js
@@ -0,0 +1,351 @@
+// const attacker = {
+//   toHit : 5,
+//   advantage : false,
+//   disadvantage : false,
+//   hitPercentage : undefined,
+//   critPercentage : 5,
+//   halfling  : false, // not accounted for. accuracy changes.
+//   halfOrc : false,
+//   luckyFeat : false, // not accounted for. accuracy changes.
+//   sharpshooterFeat : false,
+//   greatWeaponFeat : false,
+//   gwFightStyle : false, // not accounted for.  damage changes.
+//   damage : [{
+//     diceNum : 2,
+//     dieSize : 6,
+//     modifier : 3,
+//     damageType : 'mundane'
+//   // }, {
+//   //   diceNum : 1,
+//   //   dieSize : 8,
+//   //   modifier : 3,
+//   //   damageType : 'radiant'
+//   }]
+// };
+
+// const defender = {
+//   ac : 14,
+//   save : undefined, // not accounted for.  damage per swing changes
+//   saveStat : {
+//     advantage : false, // not accounted for.  hit and crit changes.
+//     disadvantage : false // not accounted for.  hit and crit changes.
+//     // strSave : 4
+//   },
+//   resistances : new Set(),
+//   vulnerabilities : new Set(),
+//   immunities : new Set(),
+// };
+
+function dieRoll() {
+  return Math.floor((20 * Math.random()) + 1)
+}
+
+const advantageLuckyHalflingD20 = {};
+const disadvantageLuckyHalflingD20 = {};
+const advantageLuckyD20 = {};
+const advantageHalflingD20 = {};
+const disadvantageLuckyD20 = {};
+const disadvantageHalflingD20 = {};
+const luckyHalflingD20 = {};
+const advantageD20 = {};
+const disadvantageD20 = {};
+const halflingD20 = {};
+const luckyD20 = {};
+const regularD20 = {};
+
+
+
+function regularResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+
+  while (count < times + 1) {
+    d20AsArray[dieRoll()]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    regularD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function advantageResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+
+  while (count < times + 1) {
+    d20AsArray[Math.max(dieRoll(), dieRoll())]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    advantageD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+
+}
+
+function disadvantageResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+
+  while (count < times + 1) {
+    d20AsArray[Math.min(dieRoll(), dieRoll())]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    disadvantageD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function halflingResult(times){
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let current;
+
+  while (count < times + 1) {
+    current = dieRoll()
+    current = current == 1 ? dieRoll() : current;
+    d20AsArray[current]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    halflingD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function luckyResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let current;
+
+  while (count < times + 1) {
+    current = dieRoll();
+    if (current < 8) {
+      current = Math.max(current, dieRoll());
+    }
+    d20AsArray[current]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    luckyD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function advantageLuckyHalflingResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let first;
+  let second;
+  let lucky;
+
+  while (count < times + 1) {
+    first = dieRoll();
+    first = first == 1 ? dieRoll() : first;
+    second = dieRoll();
+    second = second == 1 ? dieRoll() : second;
+
+    if (first < 8) {
+      lucky = dieRoll();
+      lucky == 1 ? dieRoll() : lucky;
+      first = Math.max(first, lucky);
+    }
+    if (second < 8) {
+      lucky = dieRoll();
+      lucky == 1 ? dieRoll() : lucky;
+      first = Math.max(second, lucky);
+    }
+    d20AsArray[Math.max(first, second)]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    advantageLuckyHalflingD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function advantageHalflingResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let first;
+  let second;
+
+  while (count < times + 1) {
+    first = dieRoll();
+    first = first == 1 ? dieRoll() : first;
+    second = dieRoll();
+    second = second == 1 ? dieRoll() : second;
+    d20AsArray[Math.max(first, second)]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    advantageHalflingD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function advantageLuckyResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let first;
+  let second;
+  let lucky;
+
+  while (count < times + 1) {
+    first = dieRoll();
+    second = dieRoll();
+
+    if (first < 8) {
+      lucky = dieRoll();
+      first = Math.max(first, lucky);
+    }
+    if (second < 8) {
+      lucky = dieRoll();
+      first = Math.max(second, lucky);
+    }
+    d20AsArray[Math.max(first, second)]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    advantageLuckyD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function luckyHalflingResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let current;
+  let lucky;
+
+  while (count < times + 1) {
+    current = dieRoll();
+    current = current == 1 ? dieRoll() : current;
+
+    if (current < 8) {
+      lucky = dieRoll();
+      lucky == 1 ? dieRoll() : lucky;
+      current = Math.max(current, lucky);
+    }
+    d20AsArray[current]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    luckyHalflingD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function disadvantageLuckyHalflingResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let first;
+  let second;
+  let lucky;
+
+  while (count < times + 1) {
+    first = dieRoll();
+    first = first == 1 ? dieRoll() : first;
+    second = dieRoll();
+    second = second == 1 ? dieRoll() : second;
+    lucky = dieRoll();
+    lucky = lucky == 1 ? dieRoll() : lucky;
+
+    d20AsArray[Math.max(Math.min(first, second), lucky)]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    disadvantageLuckyHalflingD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function disadvantageLuckyResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+
+  while (count < times + 1) {
+    d20AsArray[Math.max(Math.min(dieRoll(), dieRoll()), dieRoll())]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    disadvantageLuckyD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+function disadvantageHalflingResult(times) {
+  let sum = 0;
+  let count = 1;
+  let d20AsArray = new Array(21).fill(0);
+  let first;
+  let second;
+
+  while (count < times + 1) {
+    first = dieRoll();
+    first = first == 1 ? dieRoll() : first;
+    second = dieRoll();
+    second = second == 1 ? dieRoll() : second;
+
+    d20AsArray[Math.min(first, second)]++;
+    count++;
+  }
+
+  for (let i = 20; i > 0; i--) {
+    sum+= d20AsArray[i];
+    disadvantageHalflingD20[i] = (Math.round(sum/times * 1000) / 10);
+  }
+}
+
+regularResult(800000);
+advantageResult(800000);
+// disadvantageResult(800000);
+// halflingResult(800000);
+// luckyResult(800000);
+// advantageLuckyHalflingResult(450000);
+// advantageHalflingResult(800000);
+// advantageLuckyResult(700000);
+// luckyHalflingResult(800000);
+// disadvantageLuckyHalflingResult(700000); // d20[2] == 99.9999 which is rounding to 100
+// disadvantageLuckyResult(650000);
+// disadvantageHalflingResult(800000);
+
+
+
+
+console.log('regular ', regularD20);
+console.log('advantage ', advantageD20);
+console.log('disadvantage ', disadvantageD20);
+console.log('halfling ', halflingD20);
+console.log('lucky ', luckyD20);
+console.log('advantageLuckyHalfling ', advantageLuckyHalflingD20);
+console.log('advantageHalfling ', advantageHalflingD20);
+console.log('advantageLucky ', advantageLuckyD20)
+console.log('luckyHalfling ', luckyHalflingD20);
+console.log('disadvantageLuckyHalfling ', disadvantageLuckyHalflingD20);
+console.log('disadvantageLucky ', disadvantageLuckyD20);
+console.log('disadvantageHalfling ', disadvantageHalflingD20);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+console.log('d&d accuracy quest')
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 const attacker = {
   /*
@@ -18,14 +8,14 @@ hitPercentage will be set by the accuracy function based on the target.
 accuracy function will adjust the critPercentage based on advantage/disadvantage and eventually halfing and lucky as well
   */
   toHit : 5,
-  advantage : false,
+  advantage : true,
   disadvantage : false,
   hitPercentage : undefined,
   critPercentage : 5,
-  halfling  : false, // not accounted for. accuracy changes.
+  halfling  : true,
   halfOrc : false,
-  luckyFeat : false, // not accounted for. accuracy changes.
-  sharpshooterFeat : false,
+  luckyFeat : false,
+  sharpshooterFeat : true,
   greatWeaponFeat : false,
   gwFightStyle : false, // not accounted for.  damage changes.
   damage : [{
@@ -57,10 +47,9 @@ A later update will cover spell feats, class abilities, saves, and advantage/dis
   immunities : new Set(),
 };
 
+// these were determined by running a simulation with 450k - 800k die rolls to determine percentages
+// in the case of lucky the assumption is burning max possible lucky charges to get the best possible single attack roll.
 const advantageDieRoll = {
-  /*
-Values taken from online.  Value represents cumulitive statistical        chance of rolling the key with advantage.
-  */
   1 : 100, 2 : 99.8, 3 : 99, 4 : 97.8, 5 : 96,
   6 : 93.8, 7 : 91, 8 : 87.7, 9 : 84, 10 : 79.8,
   11 : 75.1, 12 : 69.8, 13 : 63.9, 14 : 57.6, 15 : 51,
@@ -68,13 +57,73 @@ Values taken from online.  Value represents cumulitive statistical        chance
 };
 
 const disadvantageDieRoll = {
-  /*
-Values taken from online.  Value represents cumulitive statistical        chance of rolling the key with disadvantage.
-  */
-  1 : 100, 2 : 90.3, 3 : 81.1, 4 : 72.3, 5 : 64,
-  6 : 56.4, 7 : 49.2, 8 : 42.4, 9 : 36.1, 10 : 30.3,
-  11 : 24.9, 12 : 20.2, 13 : 16, 14 : 12.3, 15 : 8.9,
-  16 : 6.2, 17 : 3.9, 18 : 2.2, 19 : 1, 20 : .2
+  1: 100, 2: 90.3, 3: 81, 4: 72.2,5: 63.8,
+  6: 56.1, 7: 48.9, 8: 42.1, 9: 35.9, 10: 30.2,
+  11: 24.9, 12: 20.2, 13: 16, 14: 12.2, 15: 9,
+  16: 6.2, 17: 4, 18: 2.3, 19: 1, 20: 0.3
+};
+
+const halflingDieRoll = {
+  1: 100, 2: 99.8, 3: 94.5, 4: 89.2, 5: 83.9,
+  6: 78.7, 7: 73.5, 8: 68.2, 9: 63, 10: 57.7,
+  11: 52.4, 12: 47.2, 13: 42, 14: 36.7, 15: 31.5,
+  16: 26.3, 17: 21.1, 18: 15.8, 19: 10.5, 20: 5.3
+};
+
+const luckyDieRoll = {
+  1: 100, 2: 99.8, 3: 99, 4: 97.8, 5: 96,
+  6: 93.8, 7: 91, 8: 87.7, 9: 81, 10: 74.3,
+  11: 67.5, 12: 60.7, 13: 54, 14: 47.3, 15: 40.6,
+  16: 33.8, 17: 27.1, 18: 20.3, 19: 13.5, 20: 6.8
+};
+
+const advantageLuckyHalflingDieRoll = {
+  1: 100, 2: 100, 3: 99.4,  4: 98.4, 5: 96.8,
+  6: 94.7, 7: 92.1, 8: 88.9, 9: 86.3, 10: 83.1,
+  11: 79.1, 12: 74.5, 13: 69, 14: 62.9, 15: 56.1,
+  16: 48.6, 17: 40.3, 18: 31.3, 19: 21.6, 20: 11.1
+};
+
+const advantageHalflingDieRoll = {
+  1: 100, 2: 100, 3: 99.7, 4: 98.8, 5: 97.5,
+  6: 95.5, 7: 93, 8: 90, 9: 86.4, 10: 82.2,
+  11: 77.5, 12: 72.2, 13: 66.4, 14: 60, 15: 53.1,
+  16: 45.7, 17: 37.6, 18: 29, 19: 19.9, 20: 10.2
+};
+
+const advantageLuckyDieRoll = {
+  1: 100, 2: 99.8, 3: 99, 4: 97.8, 5: 96,
+  6: 93.8, 7: 91, 8: 87.8, 9: 85.1, 10: 81.7,
+  11: 77.7, 12: 72.9, 13: 67.5, 14: 61.5, 15: 54.6,
+  16: 47.2, 17: 39.1, 18: 30.3, 19: 21, 20: 10.8
+};
+
+const luckyHalflingDieRoll = {
+  1: 100, 2: 100, 3: 99.5, 4: 98.4, 5: 96.8,
+  6: 94.7, 7: 92, 8: 88.8, 9: 82, 10: 75.2,
+  11: 68.3, 12: 61.5, 13: 54.6, 14: 47.8, 15: 41,
+  16: 34.2, 17: 27.3, 18: 20.4, 19: 13.6, 20: 6.8
+};
+
+const disadvantageLuckyHalflingDieRoll = {
+  1: 100, 2: 100, 3: 99.4, 4: 97.8, 5: 95.3,
+  6: 92, 7: 87.8, 8: 83.1, 9: 77.7, 10: 71.8,
+  11: 65.5, 12: 59, 13: 52.2, 14: 45.3, 15: 38.3,
+  16: 31.3, 17: 24.5, 18: 17.8, 19: 11.5, 20: 5.5
+};
+
+const disadvantageLuckyDieRoll = {
+  1: 100, 2: 99.5, 3: 98.1, 4: 95.8, 5: 92.8,
+  6: 89.1, 7: 84.7, 8: 79.9, 9: 74.5, 10: 68.7,
+  11: 62.6, 12: 56.2, 13: 49.7, 14: 43, 15: 36.4,
+  16: 29.8, 17: 23.3, 18: 17, 19: 10.9, 20: 5.3
+};
+
+const disadvantageHalflingDieRoll = {
+  1: 100, 2: 99.5, 3: 89.3, 4: 79.7, 5: 70.6,
+  6: 62, 7: 54, 8: 46.5, 9: 39.7, 10: 33.3,
+  11: 27.5, 12: 22.3, 13: 17.6, 14: 13.5, 15: 9.9,
+  16: 6.9, 17: 4.4, 18: 2.5, 19: 1.1, 20: 0.3
 };
 
 function halfOrcCrit(attacker, defender) {
@@ -100,8 +149,6 @@ Vulnerable to 1D4 is better than resistant to 1D6
   });
   return total;
 }
-
-// console.log(halfOrcCrit(attacker, defender))
 
 function attackDamage(attacker, defender, hitOrCrit) {
   /*
@@ -172,20 +219,45 @@ Also adjusts the crit percentage if advantage or disadvantage
 Basic math is:
 (21 - (defender armor class - attacker toHit modifer)) * 5
 5% chance of rolling each side on the die.
-21 - 20 = 1
-1 * 5 = 5
-5% chance of rolling a 20
-advantageDieRoll and disadvantageDieRoll taken from a statistical site online.
+More complex accuracy determined by a simulator
   */
   if (attacker.sharpshooterFeat || attacker.greatWeaponFeat) {
     attacker.toHit -= 5;
   }
-  if (attacker.advantage) {
+
+  if (attacker.advantage && attacker.halfling && attacker.luckyFeat) {
+    attacker.critPercentage = advantageLuckyHalflingDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = advantageLuckyHalflingDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.disadvantage && attacker.halfling && attacker.luckyFeat) {
+    attacker.critPercentage = disadvantageLuckyHalflingDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = disadvantageLuckyHalflingDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.advantage && attacker.halfling) {
+    attacker.critPercentage = advantageHalflingDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = advantageHalflingDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.disadvantage && attacker.halfling) {
+    attacker.critPercentage = disadvantageHalflingDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = disadvantageHalflingDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.advantage && attacker.luckyFeat) {
+    attacker.critPercentage = advantageLuckyDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = advantageLuckyDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.disadvantage && attacker.luckyFeat) {
+    attacker.critPercentage = disadvantageLuckyDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = disadvantageLuckyDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.halfling && attacker.luckyFeat) {
+    attacker.critPercentage = luckyHalflingDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = luckyHalflingDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.advantage) {
     attacker.critPercentage = advantageDieRoll[21 - attacker.critPercentage / 5];
     attacker.hitPercentage = advantageDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
   } else if (attacker.disadvantage) {
     attacker.critPercentage = disadvantageDieRoll[21 - attacker.critPercentage / 5];
     attacker.hitPercentage = disadvantageDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.luckyFeat) {
+    attacker.critPercentage = luckyDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = luckyDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
+  } else if (attacker.halfling) {
+    attacker.critPercentage = halflingDieRoll[21 - attacker.critPercentage / 5];
+    attacker.hitPercentage = halflingDieRoll[defender.ac - attacker.toHit] - attacker.critPercentage;
   } else {
     attacker.hitPercentage = (21 - (defender.ac - attacker.toHit)) * 5 - attacker.critPercentage;
   }
